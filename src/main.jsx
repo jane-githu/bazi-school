14b3b24a04f1a6ed17e715fbcd60fb60aecccee0
jane-githu <jane-githu@users.noreply.github.com>
2025-12-26 17:09:31 +0800

    fix router base

--- src/main.jsx
+++ src/main.jsx
@@ -7,4 +7,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+  <BrowserRouter basename="/bazi-school">  {/* <--- 加上这一句 */}
+  <App />
+</BrowserRouter>
 )
