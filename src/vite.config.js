2cce8333aa3dbaf78347ca324188c5efe23ca62b
jane-githu <jane-githu@users.noreply.github.com>
2025-12-26 17:02:19 +0800

    修复部署配置

--- src/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/bazi-school/", // <--- 加上这一行！注意前后都有斜杠
-})
